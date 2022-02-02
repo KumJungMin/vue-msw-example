76876279632d23c8b48dbfcef9afca57802af874
KumJungMin <rmawjdals@naver.com>
2022-02-02 17:08:54 +0900

    chore: change message on handlers.js

--- my-app/src/mocks/handlers.js
+++ my-app/src/mocks/handlers.js
@@ -1,12 +1,12 @@
 // 모의 API의 동작을 지정하는 곳
-// 모든 "가짜" API 요청 핸들러를 handlers.js 안에 넣을 것
+// 모의 API 요청 핸들러를 정의
 import { rest } from "msw";
 
 export default [
   rest.get("/message", (req, res, ctx) => {
     return res(
       ctx.json({
-        message: "it works :)",
+        message: "it works!",
       })
     );
   }),
