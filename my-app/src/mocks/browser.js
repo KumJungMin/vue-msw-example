a499ad18b42f612192afb330636b9efae63894ad
KumJungMin <rmawjdals@naver.com>
2022-02-02 17:23:43 +0900

    chore: change comment on mocks files

--- my-app/src/mocks/browser.js
+++ my-app/src/mocks/browser.js
@@ -1,9 +1,4 @@
-// msw에서 제공하는 서비스워커를 이용해, handlers를 인자로 넘김
-// 서비스워커: 브라우저가 백그라운드에서 실행하는 스크립트, 오프라인에서도 서비스를 사용할 수 있도록 함
-// https://so-so.dev/web/service-worker/
-// handlers.js 의 코드를 사용하여 실제 모의 서비스 작업자를 초기화하는 장소가 될 것 입니다.
-// MSW는 개발과 테스트 모두에서 사용할 수 있으므로 두 환경의 초기화는 별도로 유지해야 하며, browsers.js 는 개발용입니다.
-
+// handlers를 이용햐 서비스워커를 초기화
 import { setupWorker } from "msw";
 import handlers from "./handlers.js";
 
--- my-app/src/mocks/handlers.js
+++ my-app/src/mocks/handlers.js
@@ -1,5 +1,4 @@
 // 모의 API의 동작을 지정하는 곳
-// 모의 API 요청 핸들러를 정의
 import { rest } from "msw";
 
 export default [
